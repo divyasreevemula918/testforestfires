62a39ac7bf630c329dd34506c7c5ccabbded4b17
divyasree <divyasree5432@gmail.com>
2026-03-27 17:21:37 +0530

    Ready for Render deployment

--- application.py
+++ application.py
@@ -27,5 +27,5 @@ def predict_datapoint():
         return render_template('home.html',results=result[0])
     else:
         return render_template('home.html')
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
